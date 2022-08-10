faf848d8978f74e68bb84d6d26ed47b6c5711a70
Prachi Kodle <prachikodle@gmail.com>
2022-08-11 02:02:19 +0530

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+      {/* Logo */}
+      <div>{/* <Image src="https://links.papareact.com/qd3" /> */}</div>
+      {/* Middle */}
+      <div></div>
+      {/* Right */}
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
